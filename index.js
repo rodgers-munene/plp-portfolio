5d22b49dcf8ed71f1413748a8b1d916e53e1d521
rodgers-munene <munenerodgers72@gmail.com>
2024-11-24 23:45:20 +0300

    animations and js functionalities

--- index.js
+++ index.js
@@ -12,3 +12,40 @@ window.addEventListener('scroll', () => {
   }
   lastScrollY = window.scrollY;
 });
+
+
+// typewriter effect
+
+const words = ["print('Hello, World!')", "print('Hola, Mundo!')", "print('Hola, Mundo!')"]
+const typingSpeed = 150
+const erasingSpeed = 100
+const delayBetweenWords = 1000
+
+let wordIndex = 0
+let charIndex = 0
+const textElement = document.getElementById("animated-text")
+
+function typewriterEffect(){
+    if(charIndex < words[wordIndex].length){
+        // add the next character
+        textElement.textContent += words[wordIndex].charAt(charIndex)
+        charIndex ++;
+        setTimeout(typewriterEffect, typingSpeed)
+    }else{
+        // erase the word after a delay
+        setTimeout(eraseEffect, delayBetweenWords)
+    }
+}
+
+function eraseEffect(){
+    if(charIndex > 0){
+        // remove the last character
+        textElement.textContent = words[wordIndex].substring(0, charIndex - 1);
+        charIndex --;
+        setTimeout(eraseEffect, erasingSpeed)
+    }else{
+        wordIndex = (wordIndex + 1) % words.length // loop through words
+        setTimeout(typewriterEffect, delayBetweenWords)
+    }
+}
+typewriterEffect()
